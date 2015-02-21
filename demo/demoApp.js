39b0e5eff0649f1f5237f1a52a4caf200f6771c7
Hannes Diercks <hi@xiphe.net>
2015-02-21 21:47:37 +0100

    test(demo): make list dynamic by exposing thingsString on input
    in order to play around with changing lists and testing the behaviour

--- demo/demoApp.js
+++ demo/demoApp.js
@@ -1,50 +1,62 @@
-/* global angular */
 angular.module('demoApp', ['kachelei']).run(function($rootScope) {
   'use strict';
 
-  $rootScope.things = [
-    {
+  var things = {
+    lorem: {
       headline: 'Lorem',
       x: 2,
       y: 1
     },
-    {
+    dolor: {
       headline: 'Dolor',
       x: 4,
       y: 2
     },
-    {
+    sit: {
       headline: 'Sit',
       x: 1,
       y: 2
     },
-    {
+    ipsum: {
       headline: 'Ipsum',
       x: 2,
       y: 1
     },
-    {
+    amed: {
       headline: 'Amed',
       x: 1,
       y: 2
     },
-    {
+    foo: {
       headline: 'Foo',
       x: 2,
       y: 2
     },
-    {
+    bar: {
       headline: 'Bar',
       x: 2,
       y: 1
     },
-    {
+    baz: {
       headline: 'Baz',
       x: 1,
       y: 2
     }
-  ];
+  };
 
+  $rootScope.things = [];
+  $rootScope.thingsString = 'lorem,dolor,sit,ipsum,amed,foo,bar,baz';
+
+  $rootScope.$watch('thingsString', function(newStr, oldStr) {
+    if (newStr === oldStr && $rootScope.things.length) { return; }
+
+    $rootScope.things.length = 0;
+    newStr.split(',').forEach(function(thing) {
+      if (things[thing]) {
+        $rootScope.things.push(things[thing]);
+      }
+    });
+  });
 
 }).constant('kachelConfig', {
   ratio: 1,

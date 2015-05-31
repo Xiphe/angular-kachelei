691669be02e7a86f4ac771a0ad1e715fca7ed88e
Hannes Diercks <hi@xiphe.net>
2015-05-31 17:21:15 +0200

    fix(kRect): dont use angular extend, it produces an empty obejct

--- src/js/factory.rect.js
+++ src/js/factory.rect.js
@@ -1,9 +1,16 @@
 /* global app */
 app.factory('kRect', function() {
   return function(element) {
-    var rect = angular.extend({}, element.getBoundingClientRect());
-    rect.height = rect.height || rect.bottom - rect.top;
-    rect.width = rect.width || rect.right - rect.left;
+    var rect = {};
+    var originalRect = element.getBoundingClientRect();
+
+    rect.bottom = originalRect.bottom;
+    rect.top = originalRect.top;
+    rect.right = originalRect.right;
+    rect.left = originalRect.left;
+    rect.height = originalRect.height || originalRect.bottom - originalRect.top;
+    rect.width = originalRect.width || originalRect.right - originalRect.left;
+
     return rect;
   };
 });
